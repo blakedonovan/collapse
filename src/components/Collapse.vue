266af0ccc250662f64bc53d6c5fb5d60a9cdd06c
Loki <blakedonovan@starbase.com>
2022-08-30 22:28:54 +0200

    init

---
+++ src/components/Collapse.vue
@@ -0,0 +1,60 @@
+/* eslint-disable */
+<template>
+     <button @click="toggle">
+    {{label}}
+    </button>
+      <div class="columns">
+        <transition name="collapse" mode="in-out">
+          <div v-if="view" class="column">
+            <slot>
+
+            </slot>
+          </div>
+        </transition>
+      </div>
+  </template>
+
+<script setup lang="ts">
+  import { defineEmits } from 'vue'
+  defineProps({
+    view:{
+      type:Boolean,
+      default:false,
+      required:true
+    },
+    label:{
+      type:String,
+      default:'',
+    }
+  })
+       
+    const emit = defineEmits(['toggle'])
+
+       const toggle = () => {
+          emit('toggle')
+       
+        }
+    </script>
+    
+    <style lang="scss" scoped>
+    
+    
+  
+    
+    .column {
+      overflow:hidden;
+      transition:flex 0.5s ease-out; 
+      height:auto;
+      flex:1;
+      background-color: transparent;
+    
+     
+    }
+    
+    .collapse {
+      &-enter,
+      &-leave-to {
+        flex: 0;
+      }
+    }
+    </style>
